7b60606ad52476c3c39017bf9883497243b0574e
slawinangsan <118531114+slawinangsan@users.noreply.github.com>
2022-12-02 12:33:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,18 @@
 
 echo date('Y-m-d H:i:s');
 echo 'Test PHP';
+
+$imie = $_POST['imie'];
+$nazwisko = $_POST['nazwisko'];
+$rok_urodzenia = $_POST['rok_urodzenia'];
+$miejsce_urodzenia = $_POST['miejsce_urodzenia'];
   
-?>
+$lata = date('Y') - $rok_urodzenia;
+ 'Cześć ' .$imie. .$nazwisko.;
+$string = 'Cześć ' .$imie. .$nazwisko. 'Urodziłem/am się w/we ' .$miejsce_urodzenia.'. Mam '.$lata.' lat.';
+ 
+echo $string
+  ?>
 
 
 </body>
