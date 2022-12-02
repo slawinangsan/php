ef970ed789035d3ea3a530604174292023718d2e
slawinangsan <118531114+slawinangsan@users.noreply.github.com>
2022-12-02 13:10:32 +0100

    Update przechwyt.php

--- przechwyt.php
+++ przechwyt.php
@@ -4,7 +4,15 @@
 (string)$nazwisko = $_POST['nazwisko'];
 (int)$rok_urodzenia = $_POST['rok_urodzenia'];
 (string)$miejsce_urodzenia = $_POST['miejsce_urodzenia'];
+(string)$email = $_POST['email'];
   
+$stare_wpisy = file_get_contents('baza.txt');
+$nowe_wpisy = $stare_wpisy.PHP_EOL.$email;
+if (file_put_contents('baza.txt',$nowe_wpisy)!==false){
+  echo 'ok';
+}else{
+  echo 'blad';
+
 (int)$lata = (int)date('Y') - $rok_urodzenia;
 
 (string)$string = 'Cześć ' .$imie. .$nazwisko. 'Urodziłem/am się w/we ' .$miejsce_urodzenia.'. Mam '.$lata.' lat.';
