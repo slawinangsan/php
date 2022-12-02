3c07321a60b3dc65aa6d2f79d90a06b73670ceb8
slawinangsan <118531114+slawinangsan@users.noreply.github.com>
2022-12-02 13:17:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <head>
 </head>
 <body>
+ 
+ <?php
+  echo date('Y-m-d H:i:s')
+ ?>
 
 <form action="przechwyt.php" method="post">
   Imie: <input type="text" name="imie" /><br />
   Nazwisko: <input type="text" name="nazwisko" /><br />
   Rok urodzenia: <input type="text" name="rok_urodzenia" /><br />
   Miejsce urodzenia: <input type="text" name="miejsce_urodzenia" /><br />
+  
+  wprowdz email: <input type="text" name="email" /><br />
   <input type="submit" value="wyślij" />
   
 </form>  
